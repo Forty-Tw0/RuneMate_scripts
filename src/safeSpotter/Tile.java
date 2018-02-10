15f6bb4d1cbdaf3e2c34bac1555043cd950eb9af
forty-tw0 <programmerjared@yahoo.com>
2018-02-10 13:36:30 -0600

    debug npcs

--- src/safeSpotter/Tile.java
+++ src/safeSpotter/Tile.java
@@ -1,64 +1,138 @@
 package safeSpotter;
 
+import com.runemate.game.api.hybrid.location.Coordinate;
+import com.runemate.game.api.hybrid.region.Npcs;
+import com.runemate.game.api.hybrid.region.Players;
 import com.runemate.game.api.hybrid.region.Region.CollisionFlags;
 
-public class Tile {
-    public int x, y;
-    public int n = 0, ne = 0, e = 0, se = 0, s = 0, sw = 0, w = 0, nw = 0; /* side/boundary properties
-        0 doesn't exist and can be walked across (tiles with nothing on them)
-        1 can not be walked through but can be shot through (a fence)
-        2 can not be walked through or shot through (a thin wall) */
-    public int solidity = 0; /*
+public class Tile{
+    Coordinate coordinate;
+    private int collisionFlags;
+
+    public Tile(Coordinate coordinate) {
+        this.coordinate = coordinate;
+        this.collisionFlags = coordinate.getCollisionFlag();
+    }
+
+    /*
         0 can be walked on (tiles with nothing on them)
         1 can not be walked but can be shot over (a table)
-        2 can not be walked or shot over (a thick castle wall) */
-
-    // create a new tile object from Region collision flags
-    public Tile(int x, int y, int collisionFlags){
-        this.x = x;
-        this.y = y;
-
-        if((collisionFlags & CollisionFlags.BLOCKING_FLOOR_OBJECT) == CollisionFlags.BLOCKING_FLOOR_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGEABLE_OBJECT) == CollisionFlags.RANGEABLE_OBJECT ||
-                (collisionFlags & CollisionFlags.OBJECT_TILE) == CollisionFlags.OBJECT_TILE){
-            this.solidity = 1;
+        2 can not be walked or shot over (a thick castle wall)
+    */
+    public int getSolidity(){
+        int solidity = 0;
+        if ((collisionFlags& CollisionFlags.BLOCKING_FLOOR_OBJECT) == CollisionFlags.BLOCKING_FLOOR_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGEABLE_OBJECT) == CollisionFlags.RANGEABLE_OBJECT ||
+                (collisionFlags& CollisionFlags.OBJECT_TILE) == CollisionFlags.OBJECT_TILE) {
+            solidity = 1;
         }
-        if((collisionFlags & CollisionFlags.BLOCKED_TILE) == CollisionFlags.BLOCKED_TILE ||
-                (collisionFlags & CollisionFlags.UNSTEPPABLE_OBJECT) == CollisionFlags.UNSTEPPABLE_OBJECT){
-            this.solidity = 2;
+        if ((collisionFlags& CollisionFlags.BLOCKED_TILE) == CollisionFlags.BLOCKED_TILE ||
+                (collisionFlags& CollisionFlags.UNSTEPPABLE_OBJECT) == CollisionFlags.UNSTEPPABLE_OBJECT) {
+            solidity = 2;
         }
+        return solidity;
+    }
 
-        //note: the sides are initialised to 0, I'm just going to assume RANGE_BLOCKING boundaries will override the normal boundaries
+    /* side/boundary properties
+        0 doesn't exist and can be walked across (tiles with nothing on them)
+        1 can not be walked through but can be shot through (a fence)
+        2 can not be walked through or shot through (a thin wall)
+    */
+    public int getN(){
+        int n = 0;
+        if ((collisionFlags& CollisionFlags.NORTH_BOUNDARY_OBJECT) == CollisionFlags.NORTH_BOUNDARY_OBJECT)
+            n = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_NORTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_NORTH_BOUNDARY_OBJECT)
+            n = 2;
+        return n;
+    }
+    public int getNE(){
+        int ne = 0;
+        if ((collisionFlags& CollisionFlags.NORTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.NORTH_EAST_BOUNDARY_OBJECT)
+            ne = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_NORTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_NORTH_EAST_BOUNDARY_OBJECT)
+            ne = 2;
+        return ne;
+    }
+    public int getE(){
+        int e = 0;
+        if ((collisionFlags& CollisionFlags.EAST_BOUNDARY_OBJECT) == CollisionFlags.EAST_BOUNDARY_OBJECT)
+            e = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_EAST_BOUNDARY_OBJECT)
+            e = 2;
+        return e;
+    }
+    public int getSE(){
+        int se = 0;
+        if ((collisionFlags& CollisionFlags.SOUTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.SOUTH_EAST_BOUNDARY_OBJECT)
+            se = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_SOUTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_SOUTH_EAST_BOUNDARY_OBJECT)
+            se = 2;
+        return se;
+    }
+    public int getS(){
+        int s = 0;
+        if ((collisionFlags& CollisionFlags.SOUTH_BOUNDARY_OBJECT) == CollisionFlags.SOUTH_BOUNDARY_OBJECT)
+            s = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_SOUTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_SOUTH_BOUNDARY_OBJECT)
+            s = 2;
+        return s;
+    }
+    public int getSW(){
+        int sw = 0;
+        if ((collisionFlags& CollisionFlags.SOUTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.SOUTH_WEST_BOUNDARY_OBJECT)
+            sw = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_SOUTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_SOUTH_WEST_BOUNDARY_OBJECT)
+            sw = 2;
+        return sw;
+    }
+    public int getW(){
+        int w = 0;
+        if ((collisionFlags& CollisionFlags.WEST_BOUNDARY_OBJECT) == CollisionFlags.WEST_BOUNDARY_OBJECT)
+            w = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_WEST_BOUNDARY_OBJECT)
+            w = 2;
+        return w;
+    }
+    public int getNW(){
+        int nw = 0;
+        if ((collisionFlags& CollisionFlags.NORTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.NORTH_WEST_BOUNDARY_OBJECT)
+            nw = 1;
+        if ((collisionFlags& CollisionFlags.RANGE_BLOCKING_NORTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_NORTH_WEST_BOUNDARY_OBJECT)
+            nw = 2;
+        return nw;
+    }
 
-        if((collisionFlags & CollisionFlags.NORTH_BOUNDARY_OBJECT) == CollisionFlags.NORTH_BOUNDARY_OBJECT) this.n = 1;
-        if((collisionFlags & CollisionFlags.NORTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.NORTH_EAST_BOUNDARY_OBJECT) this.ne= 1;
-        if((collisionFlags & CollisionFlags.EAST_BOUNDARY_OBJECT) == CollisionFlags.EAST_BOUNDARY_OBJECT) this.e = 1;
-        if((collisionFlags & CollisionFlags.SOUTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.SOUTH_EAST_BOUNDARY_OBJECT) this.se = 1;
-        if((collisionFlags & CollisionFlags.SOUTH_BOUNDARY_OBJECT) == CollisionFlags.SOUTH_BOUNDARY_OBJECT) this.s = 1;
-        if((collisionFlags & CollisionFlags.SOUTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.SOUTH_WEST_BOUNDARY_OBJECT) this.sw = 1;
-        if((collisionFlags & CollisionFlags.WEST_BOUNDARY_OBJECT) == CollisionFlags.WEST_BOUNDARY_OBJECT) this.w = 1;
-        if((collisionFlags & CollisionFlags.NORTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.NORTH_WEST_BOUNDARY_OBJECT) this.nw = 1;
-
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_NORTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_NORTH_BOUNDARY_OBJECT) this.n = 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_NORTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_NORTH_EAST_BOUNDARY_OBJECT) this.ne= 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_EAST_BOUNDARY_OBJECT) this.e = 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_SOUTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_SOUTH_EAST_BOUNDARY_OBJECT) this.se = 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_SOUTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_SOUTH_BOUNDARY_OBJECT) this.s = 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_SOUTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_SOUTH_WEST_BOUNDARY_OBJECT) this.sw = 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_WEST_BOUNDARY_OBJECT) this.w = 2;
-        if((collisionFlags & CollisionFlags.RANGE_BLOCKING_NORTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_BLOCKING_NORTH_WEST_BOUNDARY_OBJECT) this.nw = 2;
-
-        if((collisionFlags & CollisionFlags.PADDING) == CollisionFlags.PADDING ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_NORTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_NORTH_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_NORTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_NORTH_EAST_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_EAST_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_SOUTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_SOUTH_EAST_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_SOUTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_SOUTH_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_SOUTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_SOUTH_WEST_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_WEST_BOUNDARY_OBJECT ||
-                (collisionFlags & CollisionFlags.RANGE_ALLOWING_NORTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_NORTH_WEST_BOUNDARY_OBJECT
+    //I think these are out of bounds
+    public int getUnused() {
+        if ((collisionFlags& CollisionFlags.PADDING) == CollisionFlags.PADDING ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_NORTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_NORTH_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_NORTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_NORTH_EAST_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_EAST_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_SOUTH_EAST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_SOUTH_EAST_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_SOUTH_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_SOUTH_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_SOUTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_SOUTH_WEST_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_WEST_BOUNDARY_OBJECT ||
+                (collisionFlags& CollisionFlags.RANGE_ALLOWING_NORTH_WEST_BOUNDARY_OBJECT) == CollisionFlags.RANGE_ALLOWING_NORTH_WEST_BOUNDARY_OBJECT
                 ) {
-            //I don't know what these do
+            return 1;
+        }else{
+            return 0;
         }
+    }
+
+    //an npc's position is tied to the root tile, think of it as though it's legs are here
+    public int containsNPC(){
+        int type = 0;
+        if (!Npcs.newQuery().on(coordinate).targeting(Players.getLocal()).actions("Attack")
+                .filter(npc -> npc.getHealthGauge() == null ||
+                        (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)).results().isEmpty()){
+            type = 1; //this is the root tile, this is where the npc's position technically is
+        } else if (!Npcs.newQuery().targeting(Players.getLocal()).actions("Attack")
+                .filter(npc -> npc.getArea().getCoordinates().contains(coordinate) && npc.getHealthGauge() == null ||
+                        (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)).results().isEmpty()){
+            //type = 2; //this is part of the npc body which extends from the root
+        }
+        return type;
     }
 }
--- src/safeSpotter/safeSpotter.java
+++ src/safeSpotter/safeSpotter.java
@@ -1,109 +1,195 @@
 package safeSpotter;
 
+import com.runemate.game.api.hybrid.entities.Npc;
 import com.runemate.game.api.hybrid.location.Coordinate;
+import com.runemate.game.api.hybrid.queries.results.LocatableEntityQueryResults;
+import com.runemate.game.api.hybrid.region.Npcs;
 import com.runemate.game.api.hybrid.region.Players;
 import com.runemate.game.api.script.framework.LoopingBot;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class safeSpotter extends LoopingBot{
-
-
     @Override
     public void onStart(String... args){
-        setLoopDelay(2000);
+        setLoopDelay(200);
     }
+
+    //When a NPC is chasing you it will first move diagonally until it aligns with your character horizontally or vertically.
+    //If the NPC is 2-squared or even more the NPC will try to align the south-west corner.
+    //http://i.imgur.com/A8SyuPS.png
 
     @Override
     public void onLoop(){
-        System.out.println(com.runemate.game.api.hybrid.region.Region.getBase());
-        System.out.println(Players.getLocal().getPosition());
-        drawMap(collisionMap(5));
+        Tile[] map = collisionMap(5);
+        List<int[]> npcTiles = getAllNpcTiles(Npcs.newQuery().targeting(Players.getLocal()).actions("Attack")
+                .filter(npc -> npc.getHealthGauge() == null ||
+                (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)).results());
+
+        drawMap(map, npcTiles);
+
+        /**/
+        if (Players.getLocal() != null) {
+            if (Players.getLocal().getTarget() == null) {
+                if(!Npcs.newQuery().targeting(Players.getLocal()).filter(npc -> npc.getHealthGauge() == null || (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)).results().isEmpty()){
+                    System.out.println("Attacking nearest aggro'd target.");
+                    Npcs.newQuery().targeting(Players.getLocal()).filter(npc -> npc.getHealthGauge() == null || (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)).results().nearest().interact("Attack");
+                } else {
+                    System.out.println("Attacking nearest new target");
+                    //area.absolute(safeTiles)
+                    LocatableEntityQueryResults<Npc> targets = Npcs.newQuery().names("Cave horror").actions("Attack").filter(
+                            npc -> npc.getHealthGauge() == null ||
+                                    (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)
+                    ).results().sortByDistance();
+                    Npc target = targets.nearest();
+                    if (target != null) target.interact("Attack");
+                }
+            } else {
+                System.out.println("In combat.");
+                //ensure we are still safe, if not move back to a safe spot - loop this since npcs can drag us out
+                Tile[] safeTiles = findSafeSpots(map, Players.getLocal().getPosition(),
+                        Npcs.newQuery().names("Cave horror").actions("Attack").filter(
+                                npc -> npc.getHealthGauge() == null ||
+                                        (npc.getHealthGauge() != null && npc.getHealthGauge().getPercent() != 0)
+                        ).results());
+                if(!tilesContain(safeTiles, Players.getLocal().getPosition())){
+                    //safeTiles.nearestTo().click();
+                }
+
+            }
+        }
+        /**/
+    }
+
+    public List<int[]> getNpcArea(Npc npc){
+        int size = npc.getDefinition().getAreaEdgeLength();
+        List<int[]> tiles = new ArrayList<>();
+        for(int y = 0; y < size; y++){
+            for(int x = 0; x < size; x++){
+                tiles.add(new int[]{npc.getPosition().getX() + x - size + 1, npc.getPosition().getY() - y});
+            }
+        }
+        return tiles;
+    }
+
+    public List<int[]> getAllNpcTiles(LocatableEntityQueryResults<Npc> npcs){
+        List<int[]> tiles = new ArrayList<>();
+        npcs.forEach((npc) -> tiles.addAll(getNpcArea(npc)));
+        return tiles;
     }
 
     public Tile[] collisionMap(int radius) {
         Tile[] map = new Tile[(int)Math.pow(radius*2+1, 2)];
-        Coordinate center = Players.getLocal().getPosition();
-        int[][] flags = com.runemate.game.api.hybrid.region.Region.getCollisionFlags()[center.getPlane()];
 
         int i = 0;
         for (int y = -radius; y <= radius; y++) {
-            for(int x = -radius; x <= radius; x++) {
-                int xx = center.getX() + x - com.runemate.game.api.hybrid.region.Region.getBase().getX();
-                int yy = center.getY() - y - com.runemate.game.api.hybrid.region.Region.getBase().getY();
-                if(xx < flags.length && yy < flags.length && xx >= 0 && yy >= 0) {
-                    map[i++] = new Tile(center.getX() + x, center.getY() - y, flags[xx][yy]);
-                }
+            for (int x = -radius; x <= radius; x++) {
+                int xx = Players.getLocal().getPosition().getX() + x;
+                int yy = Players.getLocal().getPosition().getY() - y;
+                Coordinate c = com.runemate.game.api.hybrid.region.Region.getArea().getCoordinates().stream().filter(
+                    coordinate -> coordinate.getX() == xx && coordinate.getY() == yy).collect(Collectors.toList()).get(0);
+                map[i++] = new Tile(c);
             }
         }
         return map;
     }
 
-    public void drawMap(Tile[] tiles){
-        int size = (int)Math.sqrt(tiles.length);
+    public void drawMap(Tile[] tiles, List<int[]> npcTiles) {
+        int size = (int) Math.sqrt(tiles.length);
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
-                if (y == 0 || y == size-1) {
-                    if (tiles[y * size + x].nw == 0) System.out.print("\u001B[0m+");
-                    if (tiles[y * size + x].nw == 1) System.out.print("\u001B[32m+");
-                    if (tiles[y * size + x].nw == 2) System.out.print("\u001B[31m+");
-                    if (tiles[y * size + x].n == 0) System.out.print("\u001B[0m---");
-                    if (tiles[y * size + x].n == 1) System.out.print("\u001B[32m---");
-                    if (tiles[y * size + x].n == 2) System.out.print("\u001B[31m---");
-                    if (tiles[y * size + x].ne == 0) System.out.print("\u001B[0m+");
-                    if (tiles[y * size + x].ne == 1) System.out.print("\u001B[32m+");
-                    if (tiles[y * size + x].ne == 2) System.out.print("\u001B[31m+");
+                Tile tile = tiles[y * size + x];
+                if (y == 0 || y == size - 1) {
+                    if (tile.getNW() == 0) System.out.print("\u001B[0m+");
+                    if (tile.getNW() == 1) System.out.print("\u001B[32m+");
+                    if (tile.getNW() == 2) System.out.print("\u001B[31m+");
+                    if (tile.getN() == 0) System.out.print("\u001B[0m---");
+                    if (tile.getN() == 1) System.out.print("\u001B[32m---");
+                    if (tile.getN() == 2) System.out.print("\u001B[31m---");
+                    if (tile.getNE() == 0) System.out.print("\u001B[0m+");
+                    if (tile.getNE() == 1) System.out.print("\u001B[32m+");
+                    if (tile.getNE() == 2) System.out.print("\u001B[31m+");
                 } else {
-                    if ((tiles[y * size + x].nw > tiles[y * size + x - size].sw ? tiles[y * size + x].nw : tiles[y * size + x - size].sw) == 0)
-                        System.out.print("\u001B[0m+");
-                    if ((tiles[y * size + x].nw > tiles[y * size + x - size].sw ? tiles[y * size + x].nw : tiles[y * size + x - size].sw) == 1)
-                        System.out.print("\u001B[32m+");
-                    if ((tiles[y * size + x].nw > tiles[y * size + x - size].sw ? tiles[y * size + x].nw : tiles[y * size + x - size].sw) == 2)
-                        System.out.print("\u001B[31m+");
-                    if ((tiles[y * size + x].n > tiles[y * size + x - size].s ? tiles[y * size + x].n : tiles[y * size + x - size].s) == 0)
-                        System.out.print("\u001B[0m---");
-                    if ((tiles[y * size + x].n > tiles[y * size + x - size].s ? tiles[y * size + x].n : tiles[y * size + x - size].s) == 1)
-                        System.out.print("\u001B[32m---");
-                    if ((tiles[y * size + x].n > tiles[y * size + x - size].s ? tiles[y * size + x].n : tiles[y * size + x - size].s) == 2)
-                        System.out.print("\u001B[31m---");
-                    if ((tiles[y * size + x].ne > tiles[y * size + x - size].se ? tiles[y * size + x].ne : tiles[y * size + x - size].se) == 0)
-                        System.out.print("\u001B[0m+");
-                    if ((tiles[y * size + x].ne > tiles[y * size + x - size].se ? tiles[y * size + x].ne : tiles[y * size + x - size].se) == 1)
-                        System.out.print("\u001B[32m+");
-                    if ((tiles[y * size + x].ne > tiles[y * size + x - size].se ? tiles[y * size + x].ne : tiles[y * size + x - size].se) == 2)
-                        System.out.print("\u001B[31m+");
+                    Tile tileNear = tiles[y * size + x - size];
+                    if ((tile.getNW() > tileNear.getSW() ? tile.getNW() : tileNear.getSW()) == 0) System.out.print("\u001B[0m+");
+                    if ((tile.getNW() > tileNear.getSW() ? tile.getNW() : tileNear.getSW()) == 1) System.out.print("\u001B[32m+");
+                    if ((tile.getNW() > tileNear.getSW() ? tile.getNW() : tileNear.getSW()) == 2) System.out.print("\u001B[31m+");
+                    if ((tile.getN() > tileNear.getS() ? tile.getN() : tileNear.getS()) == 0) System.out.print("\u001B[0m---");
+                    if ((tile.getN() > tileNear.getS() ? tile.getN() : tileNear.getS()) == 1) System.out.print("\u001B[32m---");
+                    if ((tile.getN() > tileNear.getS() ? tile.getN() : tileNear.getS()) == 2) System.out.print("\u001B[31m---");
+                    if ((tile.getNE() > tileNear.getSE() ? tile.getNE() : tileNear.getSE()) == 0) System.out.print("\u001B[0m+");
+                    if ((tile.getNE() > tileNear.getSE() ? tile.getNE() : tileNear.getSE()) == 1) System.out.print("\u001B[32m+");
+                    if ((tile.getNE() > tileNear.getSE() ? tile.getNE() : tileNear.getSE()) == 2) System.out.print("\u001B[31m+");
                 }
             }
             System.out.print("\u001B[0m\n");
             for (int x = 0; x < size; x++) {
-                if (tiles[y*size+x].w == 0) System.out.print("\u001B[0m| ");
-                if (tiles[y*size+x].w == 1) System.out.print("\u001B[32m| ");
-                if (tiles[y*size+x].w == 2) System.out.print("\u001B[31m| ");
-                if (Players.getLocal().getPosition().getX() == tiles[y*size+x].x &&
-                        Players.getLocal().getPosition().getY() == tiles[y*size+x].y){
+                Tile tile = tiles[y * size + x];
+                if (tile.getW() == 0) System.out.print("\u001B[0m| ");
+                if (tile.getW() == 1) System.out.print("\u001B[32m| ");
+                if (tile.getW() == 2) System.out.print("\u001B[31m| ");
+                if(tile.getUnused() == 1 || tile.getSolidity() != 0) {
+                    if (tile.getUnused() == 1) System.out.print("\u001B[31m!");
+                    if (tile.getSolidity() == 1) System.out.print("\u001B[32m#");
+                    if (tile.getSolidity() == 2) System.out.print("\u001B[31m#");
+                } else if (Players.getLocal().getPosition().getX() == tile.coordinate.getX() &&
+                        Players.getLocal().getPosition().getY() == tile.coordinate.getY()) {
                     System.out.print("\u001B[36mO");
-                }else{
-                    if (tiles[y*size+x].solidity == 0) System.out.print("\u001B[0m ");
-                    if (tiles[y*size+x].solidity == 1) System.out.print("\u001B[32m#");
-                    if (tiles[y*size+x].solidity == 2) System.out.print("\u001B[31m#");
+                } else if(isInList(npcTiles, new int[]{tile.coordinate.getX(), tile.coordinate.getY()})) {
+                    System.out.print("\u001B[33m#");
+                /*} else if (tile.containsNPC() == 1) {
+                    System.out.print("\u001B[93m#");
+                } else if (tile.containsNPC() == 2) {
+                    System.out.print("\u001B[33m#");*/
+                } else {
+                    System.out.print("\u001B[0m ");
                 }
-                if (tiles[y*size+x].e == 0) System.out.print("\u001B[0m |");
-                if (tiles[y*size+x].e == 1) System.out.print("\u001B[32m |");
-                if (tiles[y*size+x].e == 2) System.out.print("\u001B[31m |");
+                if (tile.getE() == 0) System.out.print("\u001B[0m |");
+                if (tile.getE() == 1) System.out.print("\u001B[32m |");
+                if (tile.getE() == 2) System.out.print("\u001B[31m |");
             }
             System.out.print("\u001B[0m\n");
-            if (y == size-1){
+            if (y == size - 1) {
                 for (int x = 0; x < size; x++) {
-                    if (tiles[y*size+x].sw == 0) System.out.print("\u001B[0m+");
-                    if (tiles[y*size+x].sw == 1) System.out.print("\u001B[32m+");
-                    if (tiles[y*size+x].sw == 2) System.out.print("\u001B[31m+");
-                    if (tiles[y*size+x].s == 0) System.out.print("\u001B[0m---");
-                    if (tiles[y*size+x].s == 1) System.out.print("\u001B[32m---");
-                    if (tiles[y*size+x].s == 2) System.out.print("\u001B[31m---");
-                    if (tiles[y*size+x].se == 0) System.out.print("\u001B[0m+");
-                    if (tiles[y*size+x].se == 1) System.out.print("\u001B[32m+");
-                    if (tiles[y*size+x].se == 2) System.out.print("\u001B[31m+");
+                    Tile tile = tiles[y * size + x];
+                    if (tile.getSW() == 0) System.out.print("\u001B[0m+");
+                    if (tile.getSW() == 1) System.out.print("\u001B[32m+");
+                    if (tile.getSW() == 2) System.out.print("\u001B[31m+");
+                    if (tile.getS() == 0) System.out.print("\u001B[0m---");
+                    if (tile.getS() == 1) System.out.print("\u001B[32m---");
+                    if (tile.getS() == 2) System.out.print("\u001B[31m---");
+                    if (tile.getSE() == 0) System.out.print("\u001B[0m+");
+                    if (tile.getSE() == 1) System.out.print("\u001B[32m+");
+                    if (tile.getSE() == 2) System.out.print("\u001B[31m+");
                 }
                 System.out.print("\u001B[0m\n");
             }
         }
+    }
+
+    public static boolean isInList(List<int[]> list, int[] candidate){
+        for(int[] item : list){
+            if(Arrays.equals(item, candidate)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean tilesContain(Tile[] tiles, Coordinate c){
+        for (Tile tile: tiles) {
+            if(tile.coordinate.getY() == c.getX() && tile.coordinate.getY() == c.getY()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Tile[] findSafeSpots(Tile[] map, Coordinate player, LocatableEntityQueryResults<Npc> targets){
+        Tile[] tiles = map;
+        return tiles;
     }
 }
